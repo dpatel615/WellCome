982df5699fd485c2820291130967a66e82fa3f7a
Drusti Patel <drustipatel@Drustis-MacBook-Pro.local>
2022-04-08 19:10:34 -0400

    dashboard changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,9 +5,9 @@ const withAuth = require('../utils/auth');
 // Default GET route
 router.get('/', async (req, res) => {
   try {
-    
-    res.render('homepage', { 
-        logged_in: req.session.logged_in
+
+    res.render('homepage', {
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
@@ -16,13 +16,13 @@ router.get('/', async (req, res) => {
 
 // GET route for login page
 router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect('/homepage');
-      return;
-    }
-  
-    res.render('login');
-  });
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the homepage
-      document.location.replace('/');
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
