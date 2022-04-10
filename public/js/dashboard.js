abb5c5f50d5ff68a6240fe27cbcf27dc2c4135fe
Drusti Patel <drustipatel@Drustis-MacBook-Pro.local>
2022-04-10 00:21:45 -0400

    worked on dashboard Emotions quotes

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,21 +1,29 @@
-var videoLinks = document.querySelectorAll(".videos-link");
+var quotes =[
+  'Everything that is created begins in the mind',
+  'if physical world can affect mind but mind cannot affect physical world, then its the only one-way interaction known in science !!!',
+  'Be happy life is full of happiness',
+  'Respect other, other will Respect you',
+  'Happiness is not the absence of problems, it’s the ability to deal with them.',
+  'Be positive. Be true. Be kind.',
+  'If you are positive, you’ll see opportunities instead of obstacles.',
+  'If you are positive, you’ll see opportunities instead of obstacles.',
+  'The greatest happiness of life is the conviction that we are loved; loved for ourselves, or rather, loved in spite of ourselves.'
+]
 
-// async function relaxingVedio(event) {
-//     const getData = await fetch('/api/users/logout', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//       });
-    
-//       if (getData.ok) {
-//         document.location.replace('/');
-//       } else {
-//         alert(response.statusText);
-//       }
-//     };
+ function newQuote() {
+   var randonQuotes = Math.floor(Math.random() * (quotes.length));
+   document.getElementById('quoteEmotion').innerHTML = quotes[randonQuotes];
+ }
+//var videoLinks = document.querySelectorAll(".videos-link");
+
+
  
- async function getVideoData() {
+ async function getVideoData(event) {
+   //event prevent default()
+   const catName = event.target.getAttribute('data-id');
+
      console.log(this.getVideoData(id));
-    const response = await fetch(`/api/dashboard/${this.id}`, {
+    const response = await fetch(`/api/dashboard/${catName}`, {
                 method: 'GET',
                 headers: { 'Content-Type': 'application/json' },
               });
@@ -29,8 +37,8 @@ var videoLinks = document.querySelectorAll(".videos-link");
 
 
 
-videoLinks.forEach((link) => {
-    console.log(link);
-    link.addEventListener("click", getVideoData()); 
+videoLinks.forEach((vlink) => {
+    console.log(vlink);
+    link.addEventListener("click", getVideoData); 
 
 });
