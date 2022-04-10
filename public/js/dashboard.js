3cb51a2df1c965d0886e3d6f0cbf2feb85f4b55f
pdhende <pdhendework@gmail.com>
2022-04-09 22:42:21 -0400

    updated dashboard route to make Fetch call

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,31 +1,27 @@
-// let silder = tns({
-//     Container : "my-slider",
-//     "slideBy": "1",
-//     "speed": 400,
-//     "nav": false,
-//     autoplay: true,
-//     controls: false,
-//     autoplayButtonOutput: false,
-//     responsive: {
-//         1600: {
-//             items:8,
-//             items:7,
-//             gutter:20
-//         },
-//         1024: {
-//             itesm: 6,
-//             items: 5,
-//             gutter :20
-//         },
-//         768: {
-//             items: 4,
-//             items: 3,
-//             gutter: 20
-//         },
-//         480: {
-//             items: 2,
-//             items: 1,
-//             gutter:20
-//         }
-//     }
-// })
+var videoLinks = document.querySelectorAll(".video-links");
+
+const getVideoData = async (event) => {
+    // event.preventDefault();
+    const catName = event.target.getAttribute('data-id');
+
+    // console.log(id);
+    const response = await fetch(`/api/dashboard/${catName}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+        // body: JSON.stringify(data)
+    });
+    // const jsonVal = await response.json();
+    // console.log(jsonVal);
+    if(response.ok){
+        // console.log(jsonVal);
+        return await response.json();
+        // document.location.replace('/dashboard');
+    }else{
+        alert(response.statusText);
+    }
+};
+
+videoLinks.forEach((vlink) => {
+    console.log(vlink);
+    vlink.addEventListener("click", getVideoData);
+});
