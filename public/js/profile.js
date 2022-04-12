cbac991553a767e0f0d33b2b192bec56a299a60a
Ian Jean-Pierre <ianpierre0@gmail.com>
2022-04-12 01:16:09 -0400

    profile.js/profile.handlebars update

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,70 +1,10 @@
-jQuery(document).ready(function() {
-    Parse.$ = jQuery;
-    Parse.initialize("...", "...");
+var button = document.getElementById("entries");
+var info = document.getElementById("info");
 
-    $('.form-logout').on('submit', function (e) {
-        //Prevent Default Submit Event//
-        e.preventDefault();
 
-        console.log("Performing submit");
 
-        //logout current user//
-        if (Parse.User.current()) {
-            Parse.User.logOut();
-
-        //checking if really logged out//
-        if(Parse.user.current())
-            console.log("Failed to log out!");
-        }
-
-        //redirect to sign in
-        window.location.href = ".Sign_In.html";
-    });
+button.addEventListener("click", function buttonClick() {
+    console.log(info.value)
+    console.log("Hello"); 
 
 });
-
-let btn = document.createElement("button");
-btn.innerHTML = "Click Me";
-btn.type = "submit";
-btn.name = "entriesBtn"
-document.body.appendChild(btn);
-
-let btn = document.createElement("button");
-btn.innerHTML = "Click Me";
-btn.type = "delete";
-btn.name = "deleteBtn";
-document.body.appendChild(btn);
-
-function ConfirmDelete()
-{
-    var x = confirm("Are you sure you want to delete?");
-    if (x)
-    return true;
-    else
-    return false;
-}
-
-let btn = document.createElement("button");
-btn.innerHTML = "Click Me";
-btn.type = "update";
-btn.name = "updateBtn";
-document.body.appendChild(btn);
-
-btn.addEventListener("click", ()=>{
-    if(btn.innerText === "Old") {
-        btn.innerText = "New";
-    } else {
-        btn.innertext = "New";
-    }
-});
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'profile.handlebars'));
-  });
-  
-  app.get('/api/terms', (req, res) => res.json(termData));
-  
-  app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
-  });
