361e348b825e46bd18bfb6b28ac41a7f5c9de706
Ian Jean-Pierre <ianpierre0@gmail.com>
2022-04-12 19:05:17 -0400

    joural page updates

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,10 +1,25 @@
 var button = document.getElementById("entries");
-var info = document.getElementById("info");
+var info = document.getElementById("info").value; //.value was added// suggested by tutor
+ //var delete = document.getElementByID("delete");
+ // var update = document.getElementByID("update");
 
 
 
 button.addEventListener("click", function buttonClick() {
     console.log(info.value)
-    console.log("Hello"); 
+    console.log("Was Good"); 
 
 });
+
+function editEntry(){
+    fetch('/api/post')  // tutoring session //
+
+
+}
+ 
+async function submitBtn(){
+const response = fetch('/api/post') // tutoring session //
+
+}
+
+// was instructed to add a global scope for functions & add asynchroinous functions //
