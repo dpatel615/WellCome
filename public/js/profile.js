16ff4b2bde3cc441e9db7e15f7c91783be15f742
Ian Jean-Pierre <ianpierre0@gmail.com>
2022-04-12 19:58:04 -0400

    profile updates

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,25 +1,43 @@
 var button = document.getElementById("entries");
-var info = document.getElementById("info").value; //.value was added// suggested by tutor
+// var info = document.getElementById("jo-entry").value; //.value was added// suggested by tutor
+var info = document.querySelector('#jo-entry');
  //var delete = document.getElementByID("delete");
  // var update = document.getElementByID("update");
+var submitbttn = document.getElementById("submitBtn");
 
 
+// button.addEventListener("click", function buttonClick() {
+//     console.log(info.value)
+//     console.log("Was Good"); 
 
-button.addEventListener("click", function buttonClick() {
-    console.log(info.value)
-    console.log("Was Good"); 
+// })
 
-});
+const sendData = async (event) => {
+    try {
+    console.log("send data");
+    console.log(info);
 
-function editEntry(){
-    fetch('/api/post')  // tutoring session //
+//  const response = await fetch(``, {
+//     method: 'POST',
+//     body: JSON.stringify({info}),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
 
-
-}
- 
-async function submitBtn(){
-const response = fetch('/api/post') // tutoring session //
-
-}
+//   if (response.ok) {
+//     // document.location.replace('/profile'); 
+//     console.log(response)
+//   } else {
+//     alert('Failed to post data');
+//   }
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 // was instructed to add a global scope for functions & add asynchroinous functions //
+
+submitbttn.addEventListener("click", sendData);
+    
+    
