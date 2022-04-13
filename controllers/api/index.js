a9306bee94fda32ec07f390e08954071cd8f34dc
Stephanie Mathis <stephaniedmathis@gmail.com>
2022-04-12 21:09:41 -0400

    updated routes to get post delete from journal

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const ProfileRoutes = require('./ProfileRoutes');
 
 
 router.use('/users', userRoutes);
-
+router.use('/profiles', ProfileRoutes);
 
 module.exports = router;
