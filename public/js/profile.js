4b6171392c362a91531664d1a2856b8afd542ae3
Ian Jean-Pierre <ianpierre0@gmail.com>
2022-04-12 22:13:28 -0400

    updates

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,10 +1,10 @@
 var button = document.getElementById("entries");
-// var info = document.getElementById("jo-entry").value; //.value was added// suggested by tutor
-var info = document.querySelector('#jo-entry');
+ // var info = document.getElementById("info").value;
+// var info = document.querySelector('#jo-entry').value;
  //var delete = document.getElementByID("delete");
  // var update = document.getElementByID("update");
 var submitbttn = document.getElementById("submitBtn");
-
+// const name = document.querySelector('#project-name').value.trim();
 
 // button.addEventListener("click", function buttonClick() {
 //     console.log(info.value)
@@ -14,23 +14,25 @@ var submitbttn = document.getElementById("submitBtn");
 
 const sendData = async (event) => {
     try {
+        event.preventDefault();
+        var info = document.getElementById("jo-entry").value; // yessssss!
     console.log("send data");
     console.log(info);
 
-//  const response = await fetch(``, {
-//     method: 'POST',
-//     body: JSON.stringify({info}),
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   });
+ const response = await fetch(`/api/profiles`, {
+    method: 'POST',
+    body: JSON.stringify({info}),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-//   if (response.ok) {
-//     // document.location.replace('/profile'); 
-//     console.log(response)
-//   } else {
-//     alert('Failed to post data');
-//   }
+  if (response.ok) {
+    // document.location.replace('/profile'); 
+    console.log(response)
+  } else {
+    alert('Failed to post data');
+  }
     } catch (err) {
         console.log(err);
     }
